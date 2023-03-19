b160e521e6b39fa39f7005fe87977b227f89d973
박권익 <kinowk@naver.com>
2023-03-19 16:37:38 +0900

    test : 의존관계 자동 주입

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,13 +1,14 @@
 package hello.core.member;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
 public class MemberServiceImpl implements MemberService {
 
     /*MemberRepository(추상화), MemoryMemberRepository(구체화) 둘 다 의존*/
     private final MemberRepository memberRepository;
-
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
 
     @Override
     public void join(Member member) {
